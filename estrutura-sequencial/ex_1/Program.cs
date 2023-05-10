58653589bb7d687195c6e94fb1fd94630ec85c1d
dorianascimento <rafaelnd2001@gmail.com>
2023-05-10 20:48:32 -0300

    estrutura sequencial ex1

---
+++ estrutura-sequencial/ex_1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex_1 {
+    class Program {
+        static void Main(string[] args) {
+            int num1, num2;
+
+            Console.WriteLine("Digite dois números inteiros");
+            num1 = int.Parse(Console.ReadLine());
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"SOMA = {num1 + num2}");
+        }
+    }
+}
