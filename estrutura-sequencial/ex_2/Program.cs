1b293da0f9546821fdf938335a55b17bedd6a769
dorianascimento <rafaelnd2001@gmail.com>
2023-05-10 20:56:23 -0300

    estrutura sequencial ex2

---
+++ estrutura-sequencial/ex_2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ex_2
+{
+    class Program {
+        static void Main(string[] args) {
+            const double PI = 3.14159;
+
+            double raio, area;
+
+            Console.WriteLine("Digite o raio do circulo");
+            raio = double.Parse(Console.ReadLine());
+
+            area = PI * (Math.Pow(raio, 2));
+
+            Console.WriteLine($"A = {area.ToString("F4")}");
+        }
+    }
+}
