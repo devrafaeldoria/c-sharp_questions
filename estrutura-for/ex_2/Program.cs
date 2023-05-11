bd429a7b5f616f6c303c0e08a1a35917e016b93d
dorianascimento <rafaelnd2001@gmail.com>
2023-05-11 16:58:12 -0300

    estrutura for ex2

---
+++ estrutura-for/ex_2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex_2 {
+    class Program {
+        static void Main(string[] args) {
+            int num, fora = 0, dentro = 0;
+
+            Console.WriteLine("Quantos números inteiros serão lidos");
+            num = int.Parse(Console.ReadLine());
+
+            for(int a = 0; a < num; a++) {
+                int escolha = int.Parse(Console.ReadLine());
+
+                if(escolha >=10 && escolha <= 20) {
+                    dentro++;
+                } else {
+                    fora++;
+                }
+            }
+
+            Console.WriteLine($"{dentro} in");
+            Console.WriteLine($"{fora} out");
+        }
+    }
+}
