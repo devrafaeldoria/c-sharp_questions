5f4797839d797dfefcaf6a02ccd77bee0f3367d8
dorianascimento <rafaelnd2001@gmail.com>
2023-05-10 21:52:43 -0300

    estrutura while  ex1

---
+++ estrutura-while/ex_1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ex_1 {
+    class Program {
+        static void Main(string[] args) {
+            string senhaCorreta = "2002";
+            int valido = 0;
+
+            while(valido != 1) {
+                string senha = Console.ReadLine();
+
+                if(senha != senhaCorreta) {
+                    Console.WriteLine("Senha incorreta");
+                } else {
+                    Console.WriteLine("Senha correta");
+                    valido = 1;
+                }
+            }
+        }
+    }
+}
