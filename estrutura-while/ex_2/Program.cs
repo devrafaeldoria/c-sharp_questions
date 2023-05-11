446b886d874fbb28115f9f88a7862a0009bb7ffb
dorianascimento <rafaelnd2001@gmail.com>
2023-05-11 07:58:26 -0300

    estrutura while  ex2

---
+++ estrutura-while/ex_2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ex_2 {
+    class Program {
+        static void Main(string[] args) {
+            int x = 1, y = 1;
+
+            while (x != 0 || y != 0) {
+                Console.WriteLine("Digite valor de x e y:");
+                x = int.Parse(Console.ReadLine());
+                y = int.Parse(Console.ReadLine());
+
+                if (x > 0 && y > 0) {
+                    Console.WriteLine("primeiro");
+                } else if (x > 0 && y < 0) {
+                    Console.WriteLine("quarto");
+                } else if (x < 0 && y < 0) {
+                    Console.WriteLine("terceiro");
+                } else if (x < 0 && y > 0) {
+                    Console.WriteLine("segundo");
+                }
+            }
+        }
+    }
+}
+
