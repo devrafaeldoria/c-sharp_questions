8588ba2ace8d6ec5cca22cb66812146d72d37e4d
dorianascimento <rafaelnd2001@gmail.com>
2023-05-11 16:46:03 -0300

    estrutura for ex1

---
+++ estrutura-for/ex_1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ex_1 {
+    class Program {
+        static void Main(string[] args) {
+            int num;
+
+            Console.WriteLine("Digite um numero inteiro maior que 0 e menor que 1000");
+            num = int.Parse(Console.ReadLine());
+
+            for(int a = 0; a < num; a++) {
+                if(a % 2 != 0) {
+                    Console.WriteLine(a);
+                }
+            }
+        }
+    }
+}
