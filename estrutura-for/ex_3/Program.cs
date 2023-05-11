1278149d1e66a5b25cebd79538ffe0d78032d717
dorianascimento <rafaelnd2001@gmail.com>
2023-05-11 17:18:01 -0300

    estrutura for ex3

---
+++ estrutura-for/ex_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex_3 {
+    class Program {
+        static void Main(string[] args) {
+            int num;
+
+            Console.WriteLine("Digite a quantidade de casos para calículo da media ponderada");
+            num = int.Parse(Console.ReadLine());
+
+            for(int a = 0; a < num; a++) {
+                Console.WriteLine("Digite os tres valores para a media ponderada, um na frente do outro");
+
+                string[] numeros = Console.ReadLine().Split(" ");
+                double num1 = double.Parse(numeros[0]);
+                double num2 = double.Parse(numeros[1]);
+                double num3 = double.Parse(numeros[2]);
+
+                double media = (num1 * 2 + num2 * 3 + num3 * 5) / (2 + 3 + 5);
+
+                Console.WriteLine(media.ToString("F1"));
+            }
+        }
+    }
+}
