f370c0e230441282aee7f3548fa004a2e722a248
dorianascimento <rafaelnd2001@gmail.com>
2023-05-10 21:40:44 -0300

    estrutura sequencial ex5

---
+++ estrutura-sequencial/ex_5/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace ex_4
+{
+    class Program {
+        static void Main(string[] args) {
+            int qt1, qt2, cod1, cod2;
+            double preco1, preco2, total;
+
+            string[] valores = Console.ReadLine().Split(" ");
+            cod1 = int.Parse(valores[0]);
+            qt1 = int.Parse(valores[1]);
+            preco1 = double.Parse(valores[2]);
+
+            valores = Console.ReadLine().Split(" ");
+            cod2 = int.Parse(valores[0]);
+            qt2 = int.Parse(valores[1]);
+            preco2 = double.Parse(valores[2]);
+
+            total = (qt1 * preco1) + (qt2 * preco2);
+
+            Console.WriteLine($"VALOR A PAGAR: R$ {total}");
+        }
+    }
+}
