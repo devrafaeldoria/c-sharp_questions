af0b4b956bbd6c006f955050ba9512d2b6ef8c44
dorianascimento <rafaelnd2001@gmail.com>
2023-05-10 21:15:40 -0300

    estrutura sequencial ex4

---
+++ estrutura-sequencial/ex_4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ex_4 {
+    class Program {
+        static void Main(string[] args) {
+            int numero;
+            double salario, valorHora, horas;
+
+            Console.WriteLine("Digite o numero do funcionario");
+            numero = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite as horas de trabalho do funcionario");
+            horas = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite quantos por hora o funcionario recebe");
+            valorHora = double.Parse(Console.ReadLine());
+
+            salario = valorHora * horas;
+
+            Console.WriteLine($"NUMBER = {numero}");
+            Console.WriteLine($"SALARY = $ {salario.ToString("F2")}");
+        }
+    }
+}
